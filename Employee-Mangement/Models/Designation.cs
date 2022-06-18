fa019df74be4988275acfeeb0b5e2aec52e864e8
yamikjg4 <yamikgandhi@outlook.com>
2022-06-18 12:25:18 +0530

    Add project files.

---
+++ Employee-Mangement/Models/Designation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Mangement.Models
+{
+    public class Designation
+    {
+        public long designationid { get; set; }
+        public string designationname { get; set; }
+        public bool? flagdeleted { get; set; }
+        public virtual ICollection<EmployeeModel> emp { get; set; }
+    }
+}
---
+++ Employee-Mangement/Models/JoinModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Employee_Mangement.Models
+{
+    public class JoinModel
+    {
+        public long employeeid { get; set; }
+        public string employeename { get; set; }
+        public long designationid { get; set; }
+        public decimal salary { get; set; }
+        public string designationname { get; set; }
+
+    }
+}
